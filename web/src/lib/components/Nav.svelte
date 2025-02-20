be7199699fd0e88574d224cc5ff5d0d610b21b44
Atilio Barreda <atiliobarreda@merovingian.fios-router.home>
2025-02-19 22:52:48 -0500

    styling

--- web/src/lib/components/Nav.svelte
+++ web/src/lib/components/Nav.svelte
@@ -1,5 +1,5 @@
 <nav class="fixed top-0 left-0 w-full bg-black bg-opacity-90 backdrop-blur-sm  text-[#2E8B57]" style="z-index: 999999;">
-  <ul class="flex justify-center space-x-8 py-6 text-sm md:text-base tracking-widest font-bold">
+  <ul class="flex justify-center space-x-8 py-6 text-sm md:text-base tracking-widest ">
     <li><a href="/" class="hover:text-[#FF6347] transition-colors duration-300">ABOUT</a></li>
     <li><a href="/blog" class="hover:text-[#FF6347] transition-colors duration-300">BLOG</a></li>
     <li><a href="/" class="hover:text-[#FF6347] transition-colors duration-300">COMMUNITY</a></li>
